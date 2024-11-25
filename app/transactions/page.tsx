e7f9e71e074a0474e904e3e0332f989d85c56727
Gabriel dos Prazeres <gabrieldosprazeres@hotmail.com.br>
2024-11-25 16:22:39 -0300

    fix: resolve issues with DataTable and Popover behavior
    - Fixed a mutation issue in `DataTable` by using `JSON.parse(JSON.stringify(...))` to prevent unintended changes in `transactions`.
    - Updated the `Popover` component to explicitly use `modal={true}` for modal behavior.

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -11,7 +11,10 @@ const TransactionsPage = async () => {
         <h1 className="text-2xl font-bold">Transações</h1>
         <AddTransactionButton />
       </div>
-      <DataTable columns={transcationColumns} data={transactions} />
+      <DataTable
+        columns={transcationColumns}
+        data={JSON.parse(JSON.stringify(transactions))}
+      />
     </div>
   );
 };
