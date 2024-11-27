4700fb7a3323f64faf0345643466bdb6cea34572
Gabriel dos Prazeres <gabrieldosprazeres@hotmail.com.br>
2024-11-27 09:57:47 -0300

    refactor: improve layout and styling of dashboard components
    - Adjust spacing and padding in percentage-item and transactions-pie-chart
    - Enhance layout structure in home page for better responsiveness
    - Increase number of transactions fetched in getDashboard function
    - Implement scrollable areas for better content management
    - Update root layout to use flex and overflow properties

--- app/(home)/_components/percentage-item.tsx
+++ app/(home)/_components/percentage-item.tsx
@@ -7,8 +7,8 @@ interface PercentageItemProps {
 const PercentageItem = ({ icon, title, amount }: PercentageItemProps) => {
   return (
     <div className="flex items-center justify-between">
-      <div className="flex items-center gap-2">
-        {icon}
+      <div className="flex items-center gap-3">
+        <div className="rounded-lg bg-white bg-opacity-[3%] p-2">{icon}</div>
         <p className="text-sm font-medium text-muted-foreground">{title}</p>
       </div>
       <p className="text-sm font-bold">{amount}%</p>
--- app/_data/get-dashboard/index.ts
+++ app/_data/get-dashboard/index.ts
@@ -94,7 +94,7 @@ export const getDashboard = async (month: string) => {
     orderBy: {
       date: "desc",
     },
-    take: 10,
+    take: 15,
   });
 
   return {
