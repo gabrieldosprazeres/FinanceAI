08ddeca9ac6317248d23ed29dd729195a66c119e
Gabriel dos Prazeres <gabrieldosprazeres@hotmail.com.br>
2024-12-28 16:48:22 -0300

    feat: add Sheet component and navbar constants for improved UI structure

---
+++ app/_constants/navbar.ts
@@ -0,0 +1,6 @@
+export const menuItems = [
+  { href: "/", label: "Dashboard" },
+  { href: "/transactions", label: "Transações" },
+  { href: "/blocked", label: "Assinatura" },
+  //   { href: "/subscription", label: "Assinatura" },
+];
