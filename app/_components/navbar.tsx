2d6016f6cef2d0c018bf00db6dda0fe5507a83e2
Gabriel dos Prazeres <gabrieldosprazeres@hotmail.com.br>
2024-12-28 16:46:36 -0300

    feat: refactor navbar component with responsive design and hamburger menu

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -1,49 +1,98 @@
 "use client";
 
-import { UserButton } from "@clerk/nextjs";
-import Image from "next/image";
 import Link from "next/link";
+import Image from "next/image";
 import { usePathname } from "next/navigation";
+import { UserButton } from "@clerk/nextjs";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/app/_components/ui/sheet";
+import { Button } from "./ui/button";
+import { menuItems } from "../_constants/navbar";
 
 const Navbar = () => {
   const pathname = usePathname();
   return (
-    <nav className="flex justify-between border-b border-solid px-8 py-4">
-      <div className="flex items-center gap-10">
-        <Image src="/logo.png" alt="Finance AI" width={173} height={39} />
-        <Link
-          href="/"
-          className={
-            pathname === "/"
-              ? "font-bold text-primary"
-              : "text-muted-foreground"
-          }
-        >
-          Dashboard
+    <nav className="border-b border-solid py-2">
+      <div className="mx-auto flex flex-row items-center justify-between p-4 sm:gap-10">
+        <Link href="/" className="flex items-center space-x-3">
+          <Image src="/logo.png" alt="Finance AI" width={200} height={40} />
         </Link>
-        <Link
-          href="/transactions"
-          className={
-            pathname === "/transactions"
-              ? "font-bold text-primary"
-              : "text-muted-foreground"
-          }
-        >
-          Transações
-        </Link>
-        <Link
-          href="/subscription"
-          className={
-            pathname === "/subscription"
-              ? "font-bold text-primary"
-              : "text-muted-foreground"
-          }
-        >
-          Assinatura
-        </Link>
-      </div>
 
-      <UserButton showName />
+        <div className="hidden items-center space-x-4 max-sm:flex">
+          <UserButton />
+
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button
+                size="sm"
+                className="group inline-flex items-center justify-center rounded bg-primary-foreground hover:bg-primary-foreground md:hidden"
+              >
+                <Image
+                  src="/hamburger.svg"
+                  alt="Hamburger Menu"
+                  width={28}
+                  height={28}
+                />
+              </Button>
+            </SheetTrigger>
+
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle className="font-bold uppercase text-foreground">
+                  menu
+                </SheetTitle>
+              </SheetHeader>
+
+              <nav className="mt-6 flex flex-col space-y-4">
+                {menuItems.map((item) => (
+                  <Link
+                    key={item.href}
+                    href={item.label !== "Assinatura" ? item.href : "#"}
+                    className={`text-lg ${
+                      pathname === item.href
+                        ? "font-bold text-primary"
+                        : "text-muted-foreground"
+                    } ${item.label === "Assinatura" ? "opacity-25" : ""}`}
+                  >
+                    {item.label === "Assinatura"
+                      ? `${item.label} (Em breve)`
+                      : item.label}
+                  </Link>
+                ))}
+              </nav>
+            </SheetContent>
+          </Sheet>
+        </div>
+
+        <div className="hidden w-full justify-between sm:flex">
+          <ul className="flex space-x-8">
+            {menuItems.map((item) => (
+              <li key={item.href}>
+                <Link
+                  key={item.href}
+                  href={item.label !== "Assinatura" ? item.href : "#"}
+                  className={`text-lg ${
+                    pathname === item.href
+                      ? "font-bold text-primary"
+                      : "text-muted-foreground"
+                  } ${item.label === "Assinatura" ? "opacity-25" : ""}`}
+                >
+                  {item.label === "Assinatura"
+                    ? `${item.label} (Em breve)`
+                    : item.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+
+          <UserButton showName />
+        </div>
+      </div>
     </nav>
   );
 };
